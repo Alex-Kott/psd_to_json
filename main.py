0eb3d5c0d0ab08a116de1fac7c203734e9ee1a92
Alexey Kott <alexey.kott@gmail.com>
2019-03-03 23:13:59 +0300

    ...

--- main.py
+++ main.py
@@ -6,14 +6,35 @@ from psd_tools.api.layers import SmartObjectLayer
 
 
 def main(file_name: str):
-
     image = PSDImage.open(file_name)
 
+    data = {}
+    data['layers'] = []
     for layer in image:
-        if layer.has_clip_layers():
-            for i in layer.clip_layers:
-                if isinstance(i, SmartObjectLayer):
-                    print(i.__dict__)
+        item = {}
+
+        item['backgroundColor'] = ''  # ?
+        item['bounds'] = [layer.right,
+                          layer.top,
+                          layer.width,
+                          layer.height]
+
+        item['layername'] = layer.name
+        item['name'] = layer.name
+        item['opacity'] = layer.opacity
+        item['size'] = {
+            'height': layer.height,
+            'width': layer.width
+        }
+
+        item['']
+
+
+        # if layer.has_clip_layers():
+        #     for i in layer.clip_layers:
+        #         if isinstance(i, SmartObjectLayer):
+        #             print(i.__dict__)
+        data['layers'].append(item)
 
 
 if __name__ == "__main__":
