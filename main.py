07e751f164684a9957a6dd3c124a0a94eb7ec2b3
Alexey Kott <alexey.kott@gmail.com>
2019-03-11 02:22:19 +0300

    Add transformation dots

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import json
+import re
 import sys
 import getopt
-from typing import Union, Optional, Dict
+from typing import Union, Optional, Dict, Tuple
 from uuid import uuid4
 
 from psd_tools import PSDImage
-from psd_tools.api.layers import Layer
+from psd_tools.api.layers import Layer, SmartObjectLayer
 from psd_tools.api.smart_object import SmartObject
 from psd_tools.constants import BlendMode
+from psd_tools.psd.layer_and_mask import LayerRecord
+from psd_tools.psd.tagged_blocks import TaggedBlock, PlacedLayerData
+
+
+def truncate_prefix(name: str) -> str:
+    return re.sub('mm_[^:]+:', '', name)
+
+
+def get_transformation_dots(layer: Layer):
+    if layer.has_clip_layers():
+        for clip_layer in layer.clip_layers:
+            coords = extract_smart_object(clip_layer)
+
+            if coords is not None:
+                if len(coords) > 0:
+                    return coords
 
 
 def extract_layer(layer: Layer) -> Dict[str, str]:
@@ -18,7 +35,7 @@ def extract_layer(layer: Layer) -> Dict[str, str]:
                        layer.width,
                        layer.height],
             'layername': layer.name,
-            'name': layer.name,
+            'name': truncate_prefix(layer.name),
             'opacity': layer.opacity,
             'size': {
                 'height': layer.height,
@@ -28,7 +45,11 @@ def extract_layer(layer: Layer) -> Dict[str, str]:
             'type': 'normal',
             'visibility': layer.visible,
             'blendMode': layer.blend_mode.name,
-            'position': [layer.left, layer.top]}
+            'position': {
+                'x': layer.left,
+                'y': layer.top
+            },
+            'transformation_dots': get_transformation_dots(layer)}
 
     if layer.has_clip_layers():
         item['child_objects'] = []
@@ -36,6 +57,36 @@ def extract_layer(layer: Layer) -> Dict[str, str]:
             item['child_objects'].append(extract_layer(clip_layer))
 
     return item
+
+
+def unpack_placed_layer_data(item: PlacedLayerData) -> Tuple[float]:
+    return item.transform
+
+
+def unpack_tagged_block(block: TaggedBlock) -> Tuple[float]:
+    if type(block.data) is PlacedLayerData:
+        coords = unpack_placed_layer_data(block.data)
+
+        if coords is not None:
+            if len(coords) > 0:
+                return coords
+
+
+def extract_transformation_points(record: LayerRecord):
+    for tagged_block_id, tagged_block in record.tagged_blocks.items():
+        coords = unpack_tagged_block(tagged_block)
+
+        if coords is not None:
+            if len(coords) > 0:
+                return coords
+
+
+def extract_smart_object(layer: SmartObjectLayer):
+    coords = extract_transformation_points(layer._record)
+
+    if coords is not None:
+        if len(coords) > 0:
+            return coords
 
 
 def main(input_file: str, output_file: Optional[str] = None):
